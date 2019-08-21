5b9e6071631bc95f921fb4ee8c35d0893b877b4f
4m4r199707 <34017886+4m4r199707@users.noreply.github.com>
2019-08-21 18:31:57 +0530

    build

---
+++ build/precache-manifest.74260d5d40d25932c140e5f247aa5e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0038b2157e40ec0db5864da7fe3112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4dd12e67be93a09b7d0",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "d20dc957c6fc616e4be8",
+    "url": "/static/js/2.19516c3d.chunk.js"
+  },
+  {
+    "revision": "d4dd12e67be93a09b7d0",
+    "url": "/static/js/main.7b1dd49a.chunk.js"
+  },
+  {
+    "revision": "97bd60b2596fede11ccd",
+    "url": "/static/js/runtime~main.1e266a08.js"
+  }
+]);
